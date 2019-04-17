4bd4281c5dee33e6187e8c56bcd8bc6d96c4fa97
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-17 12:43:37 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     v= programmer_hash[:dennis_ritchie][:languages]
+     v = v.join(" ")
 end
 
 def adding_matz
